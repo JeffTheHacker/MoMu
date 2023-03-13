c4941f148f233810ed4d3697e0d6802b2ac5ec5c
Jeff He <jeffhe2022@gmail.com>
2023-03-12 18:54:13 -0700

    print

--- mini-contrast/model/bert.py
+++ mini-contrast/model/bert.py
@@ -35,6 +35,7 @@ class TextEncoder(nn.Module):
         device = input_ids.device
         typ = torch.zeros(input_ids.shape).long().to(device)
         output = self.main_model(input_ids, token_type_ids=typ, attention_mask=attention_mask)['pooler_output']  # b,d
+        print(output.shape) 
         logits = self.dropout(output)
         return logits 
 
